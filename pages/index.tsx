2f8c536d7028bb26483c9df105d6ea96115aa23a
krisadaporn <krisadaporn.p@gmail.com>
2024-02-08 17:10:51 +0700

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import clientPromise from "../lib/mongodb";
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import React from 'react';
+
 
 type ConnectionStatus = {
   isConnected: boolean;
@@ -19,9 +21,11 @@ export const getServerSideProps: GetServerSideProps<
     //
     // Then you can execute queries against your database like so:
     // db.find({}) or any of the MongoDB Node Driver commands
+    const mongoUser = 'none'//process.env.MONGODB_USER
+    const mongoPass = 'none'//process.env.MONGODB_PASS
 
     return {
-      props: { isConnected: true },
+      props: { isConnected: true, mongoUser, mongoPass}, // mongoUser, mongoPass
     };
   } catch (e) {
     console.error(e);
@@ -33,76 +37,35 @@ export const getServerSideProps: GetServerSideProps<
 
 export default function Home({
   isConnected,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  mongoUser,
+  mongoPass
+}: InferGetServerSidePropsType<typeof getServerSideProps>) { 
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Vault-Demo-Test-Dynamic-Secret</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-        </h1>
 
         {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
+          <div>
+            <h2 className="subtitle">You are connected to MongoDB</h2>
+            <h2 className="subtitle">Username is: {mongoUser}</h2>
+            <h2 className="subtitle">Password is: {mongoPass}</h2>
+          </div>
+          
         ) : (
           <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
-            for instructions.
+            You are NOT connected to MongoDB.
           </h2>
         )}
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
         .container {
           min-height: 100vh;
---
+++ pages/vault.tsx
@@ -0,0 +1,52 @@
+// pages/VaultData.js
+
+import axios from 'axios';
+
+const VaultData = ({ mongodbPass, mongodbUser}) => {
+  return (
+    <div>
+      <h1>Vault Data for illustrate purpose</h1>
+      <p>MongoDB Password: {mongodbPass}</p>
+      <p>MongoDB User: {mongodbUser}</p>
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  const vaultToken = process.env.VAULT_TOKEN;
+  const vaultUrl = process.env.VAULT_URI;
+
+  try {
+    const response = await axios.get(vaultUrl, {
+      headers: {
+        'X-Vault-Token': vaultToken
+      }
+    });
+
+    const {data} = response.data.data;
+
+    if (!data) {
+      throw new Error('Data not found');
+    }
+
+    const { MONGODB_PASS, MONGODB_USER } = data; // Destructure MONGODB_PASS and MONGODB_USER from data
+    
+    return {
+      props: {
+        mongodbUser: MONGODB_USER,
+        mongodbPass: MONGODB_PASS,
+      }
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      props: {
+        mongodbUser: null,
+        mongodbPass: null,
+      }
+    };
+  }
+}
+
+export default VaultData;
+
