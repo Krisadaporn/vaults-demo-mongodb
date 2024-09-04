2e88da7fb5ef284aec80cb85729aea21dda14c42
Krisadaporn Petchdee <krisadaporn.p@gmail.com>
2024-09-04 16:05:08 +0700

    insecure add

---
+++ pages/api/insecure.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+
+
+// The input username to be used in the query
+const username = 'someUsername';
+
+// Secure query using a parameterized query
+const query = 'SELECT * FROM users WHERE username = ?';
+
+// Execute the query with the parameter
+connection.execute(query, [username], (err, results) => {
+    if (err) {
+        console.error('Error executing query:', err);
+        return;
+    }
+
+    // Handle the results
+    console.log('User data:', results);
+});
+
+// Close the connection
+connection.end();
